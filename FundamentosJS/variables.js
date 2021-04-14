0352f4c3f5b4250014a3040551c09dda9e3b3be2
Luis Dominguez <l.dominguez.aldana@gmail.com>
2021-04-13 21:57:55 -0500

    Agregando operadores a mi archivo variables.js

---
+++ FundamentosJS/variables.js
@@ -0,0 +1,93 @@
+// ==== Tipos de datos ====
+// Boolean => true or fale
+var myBoolean = true
+// String
+var myString = "Luis"
+// Number => especifica el tipo del número
+var myIntNumber = 27
+var myFloatNumber = 1.20
+// Undefined => undefined
+var myUndefined = undefined
+//  Null => null
+var myNull = null
+// Objeto => Contienen una estructura de datos que mantienen un contexto "Diccionario"
+var myObject = {
+    nombre: "Luis",
+    avarage: 8.90
+}
+// Arreglo => Contienen una lista de datos "List", sus valores apuntan a un indice.
+var myArray = ["Manzana","Pera"]
+
+// Camel Case: la palabra empieza con minuscula, la segunda con mayuscula
+var impuestosAnuales = 200000
+// Snake Case:  se juntan las pabras con _
+var impuestos_anuales = 200000
+
+/**
+ * Ejercicio 1
+ */
+
+// String
+var firstname = "Luis"
+var lastname = "Dominguez"
+var company = "Tetra Pak"
+// Number
+var age = 33
+var phone = 4423227630
+var birthyear = 1987
+// Undefined
+var gender = undefined
+var fruit = undefined
+var runningtime = undefined
+// Null
+var hobbie = null
+var webapp = null
+var work = null
+// Boolean
+var married = false
+var single = true
+// Object
+var workobject ={
+    workhour: 8,
+    worktime: "Morning",    
+}
+var schoolobject ={
+    student: "Alfonso",
+    schooltime: "Evening",
+    grade: 10.0
+}
+var sportobj ={
+    athlete: "Ramion",
+    sport: "Running"
+}
+// Array
+var fruits = ["apple","bannana","grape"]
+var cars = ["BMW", "Mercedes", "Aston Martin"]
+var games = ["COD", "Overwatch"]
+
+/**
+ * Ejercicio 2
+ */
+
+console.log("Tipo de dato fruits =====>", typeof fruits)
+console.log("Tipo de dato sportobj =====>", typeof sportobj)
+console.log("Tipo de dato married =====>", typeof married)
+console.log("Tipo de dato hobbie =====>", typeof hobbie)
+console.log("Tipo de dato runningtime =====>", typeof runningtime)
+console.log("Tipo de dato age =====>", typeof age)
+console.log("Tipo de dato firstname =====>", typeof firstname)
+
+/**
+ * Ejercicio 3
+ */
+
+var a = 5;
+var b = 10;
+var c = 15;
+
+console.log(a+b*c)
+console.log((a+b)*c)
+console.log(b/a*c)
+console.log(b/(a*c))
+console.log(a-b+c)
+console.log(a-(b+c))
